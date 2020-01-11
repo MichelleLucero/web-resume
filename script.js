a783f91333be2067a058bc4af99257fec853ddb6
Michelle Lucero <31500626+mlucero4607@users.noreply.github.com>
2020-01-10 20:54:09 -0500

    navbar responds to outside clicks

--- script.js
+++ script.js
@@ -30,16 +30,38 @@
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
+const menu = document.getElementsByClassName('menu')[0];
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
 function toggle_menu(){
-  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
   navbarLinks.classList.toggle('active')
 }
 
 document.getElementById("toggleMenu").onclick=function(){
   toggle_menu();
 }
+//close navbar outside click
+function findActive(){
+  var isActive = navbarLinks.classList.contains('active');
+  if(isActive == true){
+    toggle_menu();
+  }
+}
+
+window.addEventListener('click', function(event){
+ var isInside = navbarLinks.contains(event.target);
+ var isMenu = menu.contains(event.target);
+
+ // if(isInside == false){
+ //   console.log('isInside false');
+ // }
+ if (isMenu == false ) {
+   findActive();
+   // toggle_menu();
+   // console.log('isMenu false');
+ }
+
+
+});
